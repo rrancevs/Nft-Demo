5661f974c8d1b42d5081c4b62b05ee227cab63e0
rrancevs <whoishimh@gmail.com>
2025-11-09 13:47:21 +0200

    Update info.js

--- info.js
+++ info.js
@@ -1,1 +1,24 @@
+function collectVisitorInfo(){
+    // const satver datus
+    const data = {
+     timestamp: new Date().toISOString(),
+    userAgent: navigator.userAgent,
+    language: navigator.language,
+    screenWidth: window.screen.width,
+    screenHeight: window.screen.height,
+    platform: navigator.platform
+    };
+
+
+    const csv = Object.keys(data).join(",") + "\n" + Object.values(data).join(",") + "\n";
+
+    const blob = new Blob([csv], {type: "test/csv"})
+    const url = URL.createObjectUrl(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "visitor_info.cvs";
+    a.click()
+    URL.revokeObjectURL(url);
+
+}
 
