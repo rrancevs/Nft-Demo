8326ed3de3f6b26591a675d8371c661537ae5368
rrancevs <whoishimh@gmail.com>
2025-11-28 19:37:23 +0200

    Update notifications.js

--- notifications.js
+++ notifications.js
@@ -16,6 +16,40 @@ document.addEventListener("DOMConetentLoaded", () => {
 
     window.buyNFT = function(id) {
         showModal('you Bought NFT #${id}! ');
+
+        const nftFiles = {
+            1: "1tzLZZnniuv4GN5oykH88pm3Qs8ArsAuD",
+             // 2: "GOOGLE_DRIVE_FILE_ID_2",
+             // 3: "GOOGLE_DRIVE_FILE_ID_3"
+        };
+        
+        if (nftFiles[id]) {
+            const a = document.createElement("a");
+             a.href = `https://drive.google.com/uc?export=download&id=${nftFiles[id]}`;
+             a.download = 'nft_${id}.jpg';
+             document.body.appendChild(a);
+             a.click();
+             document.body.removeChild(a);
+        
+
+        }
+
+
+
+
+        const buttons = document.querySelectorAll("button")
+        buttons.forEach(btn =>{
+            if (btn.textContent.includes('buy nft #${id}')){
+
+                setTimeout(() => {
+                btn.textContent = "owned";
+                btn.style.backgroundColor = "#222";
+                btn.style.color = "#00bfff"
+                btn.style.cursor = "not-allowed";
+                btn.disabled = true
+            }, 1000);
+            }
+        })
     };
 
 });
