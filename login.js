387baffe7a85d79a50a1f3e200580624dd4bfb76
rrancevs <whoishimh@gmail.com>
2025-12-07 13:51:08 +0200

    Create login.js

---
+++ login.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const loginBtn = document.getElementById("loginBtn");
+ 
+  loginBtn.addEventListener("click", async (e) => {
+    e.preventDefault();
+ 
+    const email = document.getElementById("inputEmail").value.trim();
+    const password = document.getElementById("inputPassword").value;
+
+
+    if (!email || !password) {
+    alert("please fill in all fields!");
+    return;
+
+
+    const passwordHash = await sha256(password);
+
+    // Query Supabase
+    const { data, error } = await supabase
+      .from("profiles")
+      .select("*")
+      .eq("email", email)
+      .eq("password_hash", passwordHash)
+      .single();
+ 
+    if (error || !data) {
+      alert("Invalid email or password!");
+      return;
+    }
+
+    localStorage.setItem("supaBaseSession", JSON.stringify({
+      user: { id: data.id, email: data.email },
+      walletHash: data.wallet_hash
+    }));
+
+
+      window.location.href = "index.html";
+    });
+});
+
+// SHA-256 hashing function
+async function sha256(message) {
+  const msgBuffer = new TextEncoder().encode(message);
+  const hashBuffer = await crypto.subtle.digest("SHA-256", msgBuffer);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  return hashArray.map(b => b.toString(16).padStart(2, "0")).join("");
+}
+
+    
