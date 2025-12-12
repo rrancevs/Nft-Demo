673600c621c5d963cee58fb582158d2eb6b2fdce
rrancevs <whoishimh@gmail.com>
2025-12-12 19:45:04 +0200

    Update session.js

--- session.js
+++ session.js
@@ -5,13 +5,30 @@ document.addEventListener("DOMContentLoaded", () => {
   if (!sessionData) {
 
     window.location.href = "login.html";
-
-  } else {
+    return;
+  }
 
     document.getElementById("wallet").textContent = sessionData.walletHash;
 
     //document.getElementById("user-email").textContent = sessionData.user.email;
 
+  const{data : owned, error } = await supabase
+  .from("nft_owned")
+  .select("nft_id")
+  .eq("user_id"), sessionData.user.id);
+
+  if (!error && owned.lenght > 0 ) {
+    owner.forEach(row => {
+      const id = row.nft_id;
+      const btn = document.querySelector('button[data-nft="${id}"]');
+      if (btn) {
+        btn.textContent = "Owned";
+        btn.style.backgroundColor = '#222';
+        btn.style.color = '#00bfff';
+        btn.style.cursor =   "not-allowed";
+        btn.disabled = true;
+  }
+    });
   }
 
 });
