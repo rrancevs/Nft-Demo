3b17b0e27ed98f8397b41cf3d45ca32dff810034
rrancevs <whoishimh@gmail.com>
2025-12-12 20:27:24 +0200

    Update session.js

--- session.js
+++ session.js
@@ -1,35 +1,31 @@
-document.addEventListener("DOMContentLoaded", () => {
-
+document.addEventListener("DOMContentLoaded", async () => {
   const sessionData = JSON.parse(localStorage.getItem("supabaseSession"));
  
   if (!sessionData) {
-
     window.location.href = "login.html";
     return;
   }
-
-    document.getElementById("wallet").textContent = sessionData.walletHash;
-
-    //document.getElementById("user-email").textContent = sessionData.user.email;
-
-  const{data : owned, error } = await supabase
-  .from("nft_owned")
-  .select("nft_id")
-  .eq("user_id"), sessionData.user.id);
-
-  if (!error && owned.lenght > 0 ) {
-    owner.forEach(row => {
+ 
+  document.getElementById("wallet").textContent = sessionData.walletHash;
+ 
+  // Fetch NFTs owned by user
+  const { data: owned, error } = await supabase
+    .from("nft_owned")
+    .select("nft_id")
+    .eq("user_id", sessionData.user.id);
+ 
+  if (!error && owned.length > 0) {
+    // Disable the buttons on page load
+    owned.forEach(row => {
       const id = row.nft_id;
-      const btn = document.querySelector('button[data-nft="${id}"]');
+      const btn = document.querySelector(`button[data-nft="${id}"]`);
       if (btn) {
         btn.textContent = "Owned";
-        btn.style.backgroundColor = '#222';
-        btn.style.color = '#00bfff';
-        btn.style.cursor =   "not-allowed";
+        btn.style.backgroundColor = "#222";
+        btn.style.color = "#00bfff";
+        btn.style.cursor = "not-allowed";
         btn.disabled = true;
-  }
+      }
     });
   }
-
 });
- 
