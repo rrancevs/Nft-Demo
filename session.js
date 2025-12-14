d2319007c3789901f9e05a35f363fdb1eb1921a1
rrancevs <whoishimh@gmail.com>
2025-12-14 14:04:03 +0200

    Update session.js

--- session.js
+++ session.js
@@ -6,7 +6,17 @@ document.addEventListener("DOMContentLoaded", async () => {
     window.location.href = "login.html";
     return;
   }
- 
+  
+  // 1 hour session
+  const session = new Date(sessionData.createdAt).getTime();
+  const now = Date.now();
+  const oneHour = 60 * 60 * 1000
+  if ( now - sessionStart > oneHour) {
+    localStorage.removeItem("supabaseSession");
+    window.location.href = "login.html";
+    return;
+  }
+  
   document.getElementById("wallet").textContent = sessionData.walletHash;
  
   // Fetch NFTs owned by user
@@ -29,4 +39,35 @@ document.addEventListener("DOMContentLoaded", async () => {
       }
     });
   }
+
+  const timerEL = document.getElementById("timer")
+  if (timerEl) {
+    const sessionDuration = 60 * 60 * 1000;
+    const start = Date.now();
+    const sessionEnd = start + sessionDuration;
+
+    function updateTimer(){
+      const now = Date.now();
+      const diff = sessionEnd - now;
+
+      if(diff <= 0) {
+        timerEL.textContent = "00:00";
+        localStorage.removeItem("supaBase");
+        window.location.href = "login.html";
+        return;
+      }
+
+      const minutes = Math.floor(diff / 1000 / 60).toString().padStart(2, "0");
+      const seconds = Math.floor((diff / 1000) % 60).toString().padStart(2, "0");
+      timerEl.textContent = `${minutes}:${seconds}`;
+    }
+
+    updateTimer();
+    setInterval(updateTimer,  1000);
+  }
+
+ 
+        
+    
+  
 });
